d146499e7d380fc7cd9412e3b95b984a6a69cc25
mahmoudmohey97 <34627623+mahmoudmohey97@users.noreply.github.com>
2019-04-17 23:26:47 +0200

    Delete CompanyService.java

--- service/CompanyService.java
+++
@@ -1,9 +0,0 @@
-package com.jackrutorial.service;
-
-import com.jackrutorial.model.Company;
-
-public interface CompanyService 
-{
-	public Company findCompanyByEmail(String email);
-	public void saveCompany(Company company);
-}
