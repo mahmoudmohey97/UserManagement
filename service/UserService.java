30cd4899c7c830fd554876a6fc4299300d86505a
mahmoudmohey97 <34627623+mahmoudmohey97@users.noreply.github.com>
2019-04-17 23:26:57 +0200

    Delete UserService.java

--- service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.jackrutorial.service;
-import com.jackrutorial.model.User;
-
-public interface UserService
-{
-	public User findUserByEmail(String email);
-	public void saveUser(User user);
-}
