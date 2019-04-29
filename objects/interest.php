1022ded18affed979cffdee6b0949616d1b33824
mahmoudmohey97 <34627623+mahmoudmohey97@users.noreply.github.com>
2019-04-29 23:20:33 +0200

    Add files via upload

--- objects/interest.php
+++ objects/interest.php
@@ -62,7 +62,7 @@ class Interest
 
         else
         {
-            $query = "SELECT * FROM company";
+            $query = "SELECT * FROM applicant";
             $holdAll = $this->conn->query($query)->fetchAll();
             $userId =  $holdAll[sizeof($holdAll) -1]["USERID"];
             for($i = 0 ; $i < sizeof($interestsArr) ; $i++)
@@ -110,6 +110,46 @@ class Interest
                 $query2 = "INSERT INTO ".$this->tableName." 
                            SET INTERESTS = '{$interestsArr[$i]}' ,
                                 COM_USERID = '{$id}' ";
+                $result = $this-> conn->query($query2);
+                if ($result == null)
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    function deleteApplicant($id)
+    {
+        //echo "---------->".$id ;
+        $query = "DELETE FROM interest
+                         WHERE interest.USERID ='{$id}'";
+        $var = $this->conn->query($query);
+        //$query2 = "DELETE FROM hasskills WHERE hasskills.USERID = '{$id}'";
+        //$var2 = $this->conn->query($query2);
+        if($var != null )
+            return true;
+        else
+            return false;
+    }
+
+    function updateApplicant($id)
+    {
+        $interestsArr = explode("," , $this->choosenInterests);
+        //check on size
+        if(sizeof($interestsArr) < 1 || sizeof($interestsArr) > 5)
+        {
+            //choosse if written interests meet written one
+            echo "interests num";
+            return false;
+        }
+
+        else
+        {
+            for($i = 0 ; $i < sizeof($interestsArr) ; $i++)
+            {
+                $query2 = "INSERT INTO ".$this->tableName." 
+                           SET INTERESTS = '{$interestsArr[$i]}' ,
+                                USERID = '{$id}' ";
                 $result = $this-> conn->query($query2);
                 if ($result == null)
                     return false;
